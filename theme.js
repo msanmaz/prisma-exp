3ddd57775302a925b4956cf83bffcae89514a1ed
msanmaz <osanmaz4992@gmail.com>
2022-05-12 01:58:54 +0100

    prisma client

---
+++ theme.js
@@ -0,0 +1,33 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+import { mode } from '@chakra-ui/theme-tools';
+
+
+const breakpoints = createBreakpoints({
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1560px",
+})
+
+
+// const styles = {
+//   global: props => ({
+//     body: {
+//       bg: mode('white', '#161616')(props)
+//     }
+//   })
+// }
+ const config = {
+   initialColorMode: 'light',
+  useSystemColorMode: false
+
+ }
+
+const theme = extendTheme({
+    config,
+  breakpoints,
+})
+
+export default theme
